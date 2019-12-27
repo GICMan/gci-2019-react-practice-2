0f7e073725aef7884f897244efcb3b31922e8e5a
Gabriel Casselman <gicasselman1@gmail.com>
2019-12-27 09:38:26 -0800

    [GCI-GICMan] Added products and categories and created routes to get them

--- app.js
+++ app.js
@@ -4,12 +4,38 @@ const port = process.env.PORT || 3000;
 
 const packageInfo = require("./package.json");
 
+const { categories } = require("./data/categories.json");
+const { products } = require("./data/products.json");
+
+const productsFull = products.map(x => {
+  categoryName = categories.find(c => x.categoryId === c.id).categoryName;
+  return {
+    ...x,
+    categoryName
+  };
+});
+
 app.get("/", (req, res) => res.send("Hello World!"));
+
 app.get("/info", (req, res) =>
   res.json({
     serverName: packageInfo.name,
     serverVersion: packageInfo.version
   })
 );
+
+app.get("/products/all", (req, res) => {
+  res.json(productsFull);
+});
+
+app.get("/products/:id", (req, res) => {
+  product = productsFull.find(p => req.params.id == p.id);
+  res.json(product);
+});
+
+app.get("/category/:id", (req, res) => {
+  specificProducts = productsFull.filter(p => p.categoryId === req.params.id);
+  res.json(specificProducts);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
